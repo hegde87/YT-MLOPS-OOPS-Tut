65959e871970b13f3a46c2d363ffb7191f0db03a
Vikash <vikashdas770@gmail.com>
2024-07-29 16:30:02 +0530

    encapsulation, getter setter and static method covered

--- oops1.py
+++ oops1.py
@@ -2,24 +2,31 @@
 class employee:
     # special method/magic method/dunder method - constructor
     def __init__(self):
-        print("Started executing attributes/data")
+        # print(id(self))
+        # print("Started executing attributes/data")
         self.id = 123
         self.salary = 50000
         self.designation = "SDE"
-        print("attributes/data have been initiated")
+        # print("attributes/data have been initiated")
 
-    def travel(self, destination):
+    def travel(self):
         print("This travel method was called manually")
-        print(f"Employee is now travelling to {destination}")
+        print(f"Employee is now travelling to Delhi")
 
 
 # create an obj/instance of the class
 sam = employee()
+# sam.name = "Sam Kumar"
+# print(id(sam))
+# print(sam.name)
+
+# shaktiman = employee()
+# print(id(shaktiman))
 
 # printing the attributes
 # print(sam.id)
 
 # calling a method
-# sam.travel("Kerala")
+# sam.travel()
 
-print(type(sam))
+# print(type(sam))
