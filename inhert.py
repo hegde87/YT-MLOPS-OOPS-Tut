2fdd5004f98321ed899663e29efcdf9e5944dc58
Sagar Hegde <sagar.10717753@ltimindtree.com>
2025-10-19 15:53:48 +0530

    basic of inheritance

--- inhert.py
+++ inhert.py
@@ -1,27 +1,31 @@
 # Simple inheritance
 
 # # Base class
-# class Animal:
-#     def __init__(self, name):
-#         self.name = name
+class Animal:
+    def __init__(self, name):
+        self.name = name
 
-#     def speak(self):
-#         print(f"{self.name} makes a sound.")
+    def speak(self):
+        print(f"{self.name} makes a sound.")
 
 # # # Derived class
-# class Dog(Animal):
+class Dog(Animal):                      # here, name of class is "Dog" / as a Argument we are passing -->Animal (class)
 
-#     def speak(self):
-#         print(f"{self.name} barks.")
-
-# # # Create an instance of Animal
-# # animal = Animal("Generic Animal")
-# # animal.speak()  # Output: Generic Animal makes a sound.
+    def speak(self):                    # this is a method 
+        print(f"{self.name} barks.")    # this is using a Attribute called -->self.name 
+ 
+# # # Create an instance of Animal i.e creating a Object 
+animal = Animal("Generic Animal")
+animal.speak()  # Output: Generic Animal makes a sound.
 
 # # # Create an instance of Dog
-# dog = Dog()
-# dog.speak()  # Output: Buddy barks.
+dog = Dog("Buddy")
+dog.speak()  # Output: Buddy barks.
 
+#   ::::::NOTE::::::::::
+# class Dog(Animal):  --> In "Dog" class we are Inheriting "Animal" class   
+# so now from Dog class -->what ever object -->it will be able to access the atribute of Amimal class i.e [parent class]
+# And also it can inherit the method of Parent class 
 
 # Super Keyword
 
@@ -38,7 +42,7 @@ class Animal:
 # # Derived class
 class Dog(Animal):
     def __init__(self, breed):
-        super().__init__()
+        super().__init__()  # if we want to use parent attributes also then we use Super Keyword # Full constructor is called 
         self.breed = breed
 
     def speak(self):
